714bb766e820b60ebf7420c22a5816bf5749c902
asishs <asishantony@gmail.com>
2021-05-30 16:57:44 +0530

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\LanguageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Page Route
+// Route::get('/', [PageController::class, 'blankPage'])->middleware('verified');
+Route::get('/', [PageController::class, 'blankPage']);
+
+Route::get('/page-blank', [PageController::class, 'blankPage']);
+Route::get('/page-collapse', [PageController::class, 'collapsePage']);
+
+// locale route
+Route::get('lang/{locale}', [LanguageController::class, 'swap']);
+
+Auth::routes(['verify' => true]);
