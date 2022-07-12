73e39175e782d8b21f7f18363dadd059bb4135ee
Asish K Antony <asishantony@gmail.com>
2022-07-13 02:52:33 +0530

    Speed improve the performance

--- resources/views/client/layouts/client.blade.php
+++ resources/views/client/layouts/client.blade.php
@@ -17,14 +17,14 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{asset('vendors/client/animate.css/animate.min.css')}}" rel="stylesheet">
-  <link href="{{asset('vendors/client/aos/aos.css')}}" rel="stylesheet">
-  <link href="{{asset('vendors/client/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{asset('vendors/client/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{asset('vendors/client/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{asset('vendors/client/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
-  <link href="{{asset('vendors/client/remixicon/remixicon.css')}}" rel="stylesheet">
-  <link href="{{asset('vendors/client/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+  <link href="{{asset('vendors/client/animate.css/animate.min.css')}}" rel="stylesheet" async defer>
+  <link href="{{asset('vendors/client/aos/aos.css')}}" rel="stylesheet" async defer>
+  <link href="{{asset('vendors/client/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" async defer>
+  <link href="{{asset('vendors/client/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet" async defer>
+  <link href="{{asset('vendors/client/boxicons/css/boxicons.min.css')}}" rel="stylesheet" async defer>
+  <link href="{{asset('vendors/client/glightbox/css/glightbox.min.css')}}" rel="stylesheet" async defer>
+  <link href="{{asset('vendors/client/remixicon/remixicon.css')}}" rel="stylesheet" async defer>
+  <link href="{{asset('vendors/client/swiper/swiper-bundle.min.css')}}" rel="stylesheet" async defer>
 <link rel="stylesheet" type="text/css" href="{{asset('vendors/quill/quill.snow.css')}}">
 <link type="text/css" rel="stylesheet" href="css/lightgallery.css" />
 
@@ -60,16 +60,16 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendors JS Files -->
-  <script src="{{asset('vendors/client/aos/aos.js')}}"></script>
-  <script src="{{asset('vendors/client/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('vendors/client/glightbox/js/glightbox.min.js')}}"></script>
-  <script src="{{asset('vendors/client/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{asset('vendors/client/php-email-form/validate.js')}}"></script>
-  <script src="{{asset('vendors/client/purecounter/purecounter.js')}}"></script>
-  <script src="{{asset('vendors/client/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('vendors/client/aos/aos.js')}}" async defer></script>
+  <script src="{{asset('vendors/client/bootstrap/js/bootstrap.bundle.min.js')}}" async defer></script>
+  <script src="{{asset('vendors/client/glightbox/js/glightbox.min.js')}}" async defer></script>
+  <script src="{{asset('vendors/client/isotope-layout/isotope.pkgd.min.js')}}" async defer></script>
+  <script src="{{asset('vendors/client/php-email-form/validate.js')}}" async defer></script>
+  <script src="{{asset('vendors/client/purecounter/purecounter.js')}}" async defer></script>
+  <script src="{{asset('vendors/client/swiper/swiper-bundle.min.js')}}" async defer></script>
 
   <!-- Main JS File -->
-  <script src="{{asset('/js/client/main.js')}}"></script>
+  <script src="{{asset('/js/client/main.js')}}" async defer></script>
 
 </body>
 
