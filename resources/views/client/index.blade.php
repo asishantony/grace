7b7d1541f947ea392c79cdd3f5a160c62255185c
Asish K Antony <asishantony@gmail.com>
2023-07-08 22:10:38 +0530

    Testing git pull webhook

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -566,7 +566,7 @@
 
   </main><!-- End #main -->
 
-  <!-- ======= Footer git pull success ======= -->
+  <!-- ======= Footer  ======= -->
  @include("client.layouts.footer")
 
   <!-- <div id="preloader"></div> -->
